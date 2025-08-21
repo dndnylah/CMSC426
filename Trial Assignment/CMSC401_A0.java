d1f1a10006f72cc3abf8e03752b9263a308ab523
Nylah B <nylahbelk3@gmail.com>
2025-08-21 14:16:47 -0400

    Create and Complete Trial Assignment for CMSC401

---
+++ Trial Assignment/CMSC401_A0.java
@@ -0,0 +1,39 @@
+/** Nylah Belk **/
+import java.util.Scanner;
+
+public class CMSC401_A0{
+    public static void main(String[] args){
+        // import scanner
+        try (Scanner sc = new Scanner(System.in)) {
+            // read in the value associated with line numbers
+            String count = sc.nextLine();
+            // convert count value to an integer
+            int c = Integer.parseInt(count);
+            // an empty array to store the sums, with the length of c
+            int[] sums = new int[c];
+            // a for loop to read in each line
+            for (int i = 0; i < c; i++){
+                String line = sc.nextLine();
+                // split the read in line by space
+                String[] numbers = line.split(" ");
+                // an empty array to store numercial forms of numbers string
+                int[] num = new int[numbers.length];
+                // convert the numbers array to integers
+                for (int j = 0; j < numbers.length; j++){
+                    num[j] = Integer.parseInt(numbers[j]);
+                }
+                // identify the x,y values for the array positions to be summed
+                int first = num[num.length - 2];
+                int second = num[num.length - 1];
+                // sum the values in the x, y positions
+                int sum = num[first] + num[second];
+                // add each sum to the array at their given line
+                sums[i] = sum;
+            }
+            // print each sum value in order, separated by a line
+            for (int i = 0; i < sums.length; i++){
+                System.out.println(sums[i]);
+            }
+        }
+    }
+}
